6bc24cb7b6791c33ca9b8b9ade713ddbd204d015
Md Hasanuzzaman <hasanhira.me@gmail.com>
2025-10-26 20:33:34 +0600

    WP basic theme files added

---
+++ functions.php
@@ -0,0 +1,99 @@
+<?php 
+if ( ! function_exists( 'twentyfifteen_setup' ) ) :
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * Note that this function is hooked into the after_setup_theme hook, which
+     * runs before the init hook. The init hook is too late for some features, such
+     * as indicating support for post thumbnails.
+     *
+     * @since Twenty Fifteen 1.0
+     */
+    function twentyfifteen_setup() {
+    
+        /*
+         * Make theme available for translation.
+         * Translations can be filed in the /languages/ directory.
+         * If you're building a theme based on twentyfifteen, use a find and replace
+         * to change 'twentyfifteen' to the name of your theme in all the template files
+         */
+        load_theme_textdomain( 'twentyfifteen', get_template_directory() . '/languages' );
+    
+        // Add default posts and comments RSS feed links to head.
+        add_theme_support( 'automatic-feed-links' );
+    
+        /*
+         * Let WordPress manage the document title.
+         * By adding theme support, we declare that this theme does not use a
+         * hard-coded  tag in the document head, and expect WordPress to
+         * provide it for us.
+         */
+        add_theme_support( 'title-tag' );
+    
+        /*
+         * Enable support for Post Thumbnails on posts and pages.
+         *
+         * See: https://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+         */
+        add_theme_support( 'post-thumbnails' );
+        set_post_thumbnail_size( 825, 510, true );
+    
+        // This theme uses wp_nav_menu() in two locations.
+        register_nav_menus( array(
+            'primary' =&gt; __( 'Primary Menu',      'twentyfifteen' ),
+            'social'  =&gt; __( 'Social Links Menu', 'twentyfifteen' ),
+        ) );
+    
+        /*
+         * Switch default core markup for search form, comment form, and comments
+         * to output valid HTML5.
+         */
+        add_theme_support( 'html5', array(
+            'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+        ) );
+    
+        /*
+         * Enable support for Post Formats.
+         *
+         * See: https://codex.wordpress.org/Post_Formats
+         */
+        add_theme_support( 'post-formats', array(
+            'aside', 'image', 'video', 'quote', 'link', 'gallery', 'status', 'audio', 'chat'
+        ) );
+    
+        $color_scheme  = twentyfifteen_get_color_scheme();
+        $default_color = trim( $color_scheme[0], '#' );
+    
+        // Setup the WordPress core custom background feature.
+        add_theme_support( 'custom-background', apply_filters( 'twentyfifteen_custom_background_args', array(
+            'default-color'      =&gt; $default_color,
+            'default-attachment' =&gt; 'fixed',
+        ) ) );
+    
+        /*
+         * This theme styles the visual editor to resemble the theme style,
+         * specifically font, colors, icons, and column width.
+         */
+        add_editor_style( array( 'css/editor-style.css', 'genericons/genericons.css', twentyfifteen_fonts_url() ) );
+    }
+    endif;
+    // twentyfifteen_setup
+    add_action( 'after_setup_theme', 'twentyfifteen_setup' );
+
+
+
+
+    function theme_scripts() {
+    // css 
+    wp_enqueue_style( 'kindaid-main', get_template_directory_uri() . '/path/css/main.css', array(), '1.0', 'all' );
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+    // js
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/path/js/bootstrap-min.js', array( 'jquery' ), '5.3.8', true );
+    
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+// kindaid enqueue scripts
+add_action( 'wp_enqueue_scripts', 'theme_scripts' );
